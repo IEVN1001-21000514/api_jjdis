41302e11143647e459ed9a5a18482aa8115d92f6
“Kevin <gomezyahirmtz2@gmail.com>
2025-03-26 13:18:31 -0600

    BD

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 class DevelopmentConfig():
     DEBUG = True
-    MYSQL_HOST = 'localhost'
-    MYSQL_USER = 'root'
-    MYSQL_PASSWORD = ''  # Cambia si tienes contraseña en MySQL
-    MYSQL_DB = 'jjdis'  # Asegúrate de que este nombre coincida con tu base de datos
+    MYSQL_HOST = 'sql207.infinityfree.com'
+    MYSQL_USER = 'if0_38604025'
+    MYSQL_PASSWORD = '22Kevin100202'  # Cambia si tienes contraseña en MySQL
+    MYSQL_DB = 'if0_38604025_XXX'  # Asegúrate de que este nombre coincida con tu base de datos
 
 config = {
     'development': DevelopmentConfig
